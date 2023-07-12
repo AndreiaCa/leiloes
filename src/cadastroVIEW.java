e48e8821d7bfe259e928e18c7df2105b2740e831
Andreia Santos <137791508+AndreiaCa@users.noreply.github.com>
2023-07-12 09:44:16 -0300

    Adicao do método quantidade de cadastro

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -15,7 +15,10 @@ public class cadastroVIEW extends javax.swing.JFrame {
     public cadastroVIEW() {
         initComponents();
     }
-
+     public void cadastroQuantidade(){
+         
+     };
+             
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
